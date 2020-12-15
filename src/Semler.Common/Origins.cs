ea718866075c64e83fe2bd0ef3192ea55696c147
Dennis Meinert Pedersen <dmp@kapacity.dk>
2020-12-15 11:59:09 +0100

    Include common static for referencing systems

---
+++ src/Semler.Common/Origins.cs
@@ -0,0 +1,16 @@
+﻿namespace CluedIn.Semler.Common
+{
+    public static class Origins
+    {
+        static Origins()
+        {
+            KUK = "KUK";
+            Geomatic = "Geomatic";
+            Salesforce = "Salesforce";
+        }
+
+        public static string KUK { get; private set; }
+        public static string Geomatic { get; private set; }
+        public static string Salesforce { get; private set; }
+    }
+}
