71cdd1190f93d0308374b0cfc5dfe8ead6093b25
Dennis Meinert Pedersen <dmp@kapacity.dk>
2020-12-15 12:08:11 +0100

    Add fields which might be needed, but not determined yet

--- src/Semler.Common/Origins.cs
+++ src/Semler.Common/Origins.cs
@@ -7,10 +7,14 @@
             KUK = "KUK";
             Geomatic = "Geomatic";
             Salesforce = "Salesforce";
+            Cvr = "cvr";
+            Cpr = "cpr";
         }
 
         public static string KUK { get; private set; }
         public static string Geomatic { get; private set; }
         public static string Salesforce { get; private set; }
+        public static string Cvr { get; private set; }
+        public static string Cpr { get; private set; }
     }
 }
