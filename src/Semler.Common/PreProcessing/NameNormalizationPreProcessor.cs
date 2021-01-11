2aba32620db665519ee45354f9b507b25fe26d46
Ziggy <zygis0408@gmail.com>
2021-01-11 13:16:43 +0100

    added example preprocessor

---
+++ src/Semler.Common/PreProcessing/NameNormalizationPreProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CluedIn.Core;
+using CluedIn.Core.Data;
+using CluedIn.Core.Data.Parts;
+
+namespace CluedIn.Semler.Common.PreProcessing
+{
+    public class NameNormalizationPreProcessor : Processing.Processors.PreProcessing.IPreProcessor
+    {
+        public bool Accepts(ExecutionContext context, IEnumerable<IEntityCode> codes)
+        {
+            return true;
+        }
+
+        public void Process(ExecutionContext context, IEntityMetadataPart metadata, IDataPart data)
+        {
+            if (metadata != null)
+            {
+                metadata.DisplayName = metadata.DisplayName.ToLower().ToTitleCase();
+            }
+        }
+    }
+}
