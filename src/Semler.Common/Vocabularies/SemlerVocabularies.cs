7e974f740902aa8fd6b5c6f0aebc154c13d7241a
Azmir Salihovic <asa@kapacity.dk>
2021-01-13 11:57:55 +0100

    Vocabulary performance fix

--- src/Semler.Common/Vocabularies/SemlerVocabularies.cs
+++ src/Semler.Common/Vocabularies/SemlerVocabularies.cs
@@ -2,9 +2,9 @@
 {
     public static class SemlerVocabularies
     {
-        public static BusinessCustomerVocabulary BusinessCustomer => new BusinessCustomerVocabulary();
-        public static ContactVocabulary Contact => new ContactVocabulary();
-        public static CustomerVocabulary Customer => new CustomerVocabulary();
-        public static PrivateCustomerVocabulary PrivateCustomer => new PrivateCustomerVocabulary();
+        public static CustomerVocabulary Customer = new CustomerVocabulary();
+        public static BusinessCustomerVocabulary BusinessCustomer = new BusinessCustomerVocabulary();
+        public static PrivateCustomerVocabulary PrivateCustomer = new PrivateCustomerVocabulary();
+        public static ContactVocabulary Contact = new ContactVocabulary();
     }
 }
