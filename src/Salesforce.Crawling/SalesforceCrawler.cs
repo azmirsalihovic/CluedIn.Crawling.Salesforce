a3ab85652515df77990a7ea384edbc7a82b1618d
Azmir Salihovic <asa@kapacity.dk>
2021-02-22 14:22:17 +0100

    Removed export of CustomerIDs

--- src/Salesforce.Crawling/SalesforceCrawler.cs
+++ src/Salesforce.Crawling/SalesforceCrawler.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using CluedIn.Core.Crawling;
 using CluedIn.Crawling.Salesforce.Core;
 using CluedIn.Crawling.Salesforce.Core.Models;
@@ -25,9 +27,6 @@ namespace CluedIn.Crawling.Salesforce
 
             var client = _clientFactory.CreateNew(salesforcecrawlJobData);
 
-            //Get the list with customer id's from CSV file
-            //var kukCustomerIdList = client.GetKUKCustomerIDList();
-
             foreach (var item in client.GetById<Account>("Account", "'0121t000000Dy89AAC'")) //Organization
             {
                 yield return item;
@@ -37,30 +36,6 @@ namespace CluedIn.Crawling.Salesforce
             {
                 yield return item;
             }
-
-
-            //retrieve data from provider and yield objects
-            //foreach (var item in client.Get<Account>("Account", "'0122o0000007pMrAAI'")) //Person
-            //{
-            //    if (KUKCustomerIDList != null)
-            //    {
-            //        if (KUKCustomerIDList.Contains(item.KUKCustomerID__c) || KUKCustomerIDList.Count == 0)
-            //            yield return item;
-            //    }
-            //    else
-            //        yield return item;
-            //}
-
-            //foreach (var item in client.Get<Account>("Account", "'0121t000000Dy89AAC'")) //Organization
-            //{
-            //    if (KUKCustomerIDList != null)
-            //    {
-            //        if (KUKCustomerIDList.Contains(item.KUKCustomerID__c) || KUKCustomerIDList.Count == 0)
-            //            yield return item;
-            //    }
-            //    else
-            //        yield return item;
-            //}
 
             foreach (var item in client.Get<Contact>("Contact", "'0121t0000010SSoAAM'")) //Organization
             {
