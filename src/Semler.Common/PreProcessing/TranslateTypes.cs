2a85a5fd5b35d9908f449a78a2eab16bc0679329
jurgelionis <ljurgelionis@gmail.com>
2021-02-23 08:54:16 +0100

    Add temp processor to clean uo /Person

--- src/Semler.Common/PreProcessing/TranslateTypes.cs
+++ src/Semler.Common/PreProcessing/TranslateTypes.cs
@@ -8,47 +8,23 @@ using CluedIn.Core.Data.Parts;
 
 namespace Semler.Common.PreProcessing
 {
-    public class AccountTypePreProcessor : CluedIn.Processing.Processors.PreProcessing.IPreProcessor
+    public class TranslateTypesPreProcessor : CluedIn.Processing.Processors.PreProcessing.IPreProcessor
     {
 
         public bool Accepts(ExecutionContext context, IEnumerable<IEntityCode> codes)
         {
-            return codes.Any(x => x.Origin.Code == "Salesforce" || x.Origin.Code == "Geomatic");
+            return codes.Any(x => x.Origin.Code == "Salesforce" || x.Origin.Code == "Geomatic" || x.Origin.Code == "KUK");
         }
 
         public void Process(ExecutionContext context, IEntityMetadataPart metadata, IDataPart data)
         {
-            if (ConfigurationManagerEx.AppSettings.GetFlag("Semler.ClueProcessing.EntityTypeTranslation", false))
+            if (ConfigurationManagerEx.AppSettings.GetFlag("Semler.ClueProcessing.TempTranslateTypes", true))
             {
                 if (metadata != null)
                 {
-                    var kukCodes = metadata.Codes.Where(x => x.ToString().Contains("CustId"));
-                    IEntityCode code = null;
-                    if (kukCodes.Any())
+                    if (metadata.EntityType == "/Person")
                     {
-                        code = kukCodes.First();
-
-                        if (metadata.EntityType.Is(EntityType.Organization))
-                        {
-                            code = new EntityCode(EntityType.Infrastructure.User, Origins.KUK, code.Value);
-                        }
-                        else if (metadata.EntityType.Is(EntityType.Infrastructure.User))
-                        {
-                            code = new EntityCode(EntityType.Organization, Origins.KUK, code.Value);
-                        }
-
-                        var entity = context.PrimaryDataStore.GetByEntityCode(context, code);
-
-                        if (entity != null)
-                        {
-                            if (entity.ProcessedData.OriginEntityCode.Origin.Code == "KUK")
-                            {
-                                {
-                                    metadata.EntityType = entity.EntityType;
-                                    metadata.Codes.Add(new EntityCode(entity.EntityType, Origins.CustId, code.Value));
-                                }
-                            }
-                        }
+                        metadata.EntityType = "/Infrastructure/User";
                     }
                 }
             }
